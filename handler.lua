704a01299d135a2486753ecc59702c7206bbffba
Chris McGee <cmcgee.csm@gmail.com>
2019-09-27 13:44:30 -0400

    additional logging added

--- handler.lua
+++ handler.lua
@@ -8,17 +8,21 @@ end
 
 function Cookie2HeaderHandler:rewrite(config)
     Cookie2HeaderHandler.super.rewrite(self)
-    
-    local ck = cookie:new()
-    local hostName, err = ck:get(config.cookie_name)
 
-    if hostName then
-        ngx.req.set_header(config.header_key, hostName)
+    local ck = cookie:new()
+    local cookie, err = ck:get(config.cookie_name)
+
+    if cookie then
+        ngx.log(ngx.INFO, "Cookie found, writing to header.")
+        ngx.log(ngx.DEBUG, "Header: ", config.header_key)
+        ngx.log(ngx.DEBUG, "Value: ", cookie)
+        ngx.req.set_header(config.header_key, cookie)
     end
-    
+
     -- todo: possibly deprecated (or repurpose to ignore paths)
     local isLogout = config.logout_path ~= nil and not (string.find(ngx.var.uri, config.logout_path) == nil)
     if isLogout then
+        ngx.log(ngx.LOG, "isLogout path hit, clearing hearing value")
         ngx.req.clear_header(config.header_key)
     end
 end
